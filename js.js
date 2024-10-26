dc23cd847c04c734c52e6e74a160ff86237916c2
tany <49059282+ta-any@users.noreply.github.com>
2024-10-26 18:46:37 +0700

    js.js

--- js.js
+++ js.js
@@ -1,9 +1,11 @@
-let typical = {
+const typical = {
 	list_parametrs: {
 				'touch': true,
 				'dots': false,
 				'arrows': false,
 				'duration': 500,
+				'index' : 0,
+				'loop' : false,
 	}, 
 	styles: {
 		wrapper: {
@@ -28,7 +30,7 @@ let typical = {
 	},
 }
 
-let Base = {
+const Base = {
 	__proto__ : typical,
 
 	replaceDataset(data){
@@ -39,28 +41,18 @@ let Base = {
 		} 
 		return data
 	},
-	wrapper(position, contents, wrapperName = 'wrapper'){
+	wrapper(contents, wrapperName = 'wrapper'){ 
 		let newWrapper = document.createElement('div');
 		newWrapper.classList.add(wrapperName);
+
 		contents.forEach((item) => newWrapper.appendChild(item))
-		position.appendChild(newWrapper)
+		
+		return newWrapper
 	},
 	append_to_style(html_element, styles){
 		for(let name in styles){
 			html_element.style[name] = styles[name]
 		}
-	},
-	display_carusel(block){
-		let content = [...block.children]
-		this.width = this.calculateTheWidth(content)
-		this.wrapper(block, content)
-
-		let cls_wrapper = document.querySelector(`.wrapper`)
-		let footer = this.add_html_block(block, [], 'bottom', 'footer')
-
-		typical['styles']['wrapper']['width'] = `${this.width}px`
-		this.append_to_style(cls_wrapper, typical['styles']['wrapper'])
-		this.append_to_style(block, typical['styles']['id'])
 	},
 	calculateTheWidth(content){
 		let count = content.length
@@ -70,21 +62,22 @@ let Base = {
 		}
 		return width
 	},
-	add_html_block(basic, contents, position, blockName = 'addition'){
-		let newElement = this.get_array_elements(contents);
-		newElement.classList.add(blockName);
-		
-		this.append_to_style(newElement, typical['styles'][blockName])
-		
+	create_element(name){
+		let DIV = document.createElement('div')
+		DIV.classList.add(name)
+
+		return DIV
+	},
+	add_html_block(basic, content, position){
 		if(position == 'top'){
-			basic.prepend(newElement)
+			basic.prepend(content)
 		}
 		if(position == 'bottom'){
-			basic.append(newElement)
+			basic.append(content)
 		}
 		
 	},
-	get_array_elements(contents){ 
+	get_array_elements(contents){
 		let DIVs = document.createElement('div')
 		contents.forEach((item) => DIVs.appendChild(document.createElement('div')))
 		
@@ -99,98 +92,102 @@ let Base = {
 			return -1
 		}
 	},
+	clone_content(block, content, place, count = 1){
+		let clone = []
+
+		for(let i = 1; count >= i; i++){
+			content.forEach(element => {
+				clone.push(element.cloneNode(true)) 
+			})
+		}
+
+		clone.forEach(element => this.add_html_block(block, element, place))
+	}
 };
 
-class Event {
+class Feature {
+	start_shift = 0
+	width = 0
+	matrix = [] 
 	status_index = 0
-	sum = 0
-	constructor(option){
-		this.option = option;
-		this.list = [...this.option.wrapper_block.children]
-	}
-	change_img(index){
-		this.sum = 0
-		this.sum += this.list[index].offsetWidth * index
-		this.option.content.append_to_style(this.option.wrapper_block, {
-			left: `-${this.sum}px`
-		})
-		this.change_status_index(index)
-		this.current_dot()
-	}
+	core = 0
 
-	move_to(new_index){
-		let start = -(this.list[this.status_index].offsetWidth * this.status_index)
-		let step = Math.abs((this.status_index - new_index)) 
-
-		if(new_index < this.status_index ){ 
-			if(new_index == -1){
-				return -1 
-			} else {
-				this.change_status_index(new_index)
-				this.sum = this.sum - (this.list[this.status_index].offsetWidth * step)
-				this.current_dot()
-			}
-		} else if(new_index > this.status_index){ 
-			if(new_index >= this.list.length){
-				return -1 
-			} else {
-				this.change_status_index(new_index)
-				this.sum = this.sum + (this.list[this.status_index].offsetWidth * step)
-				this.current_dot()
-			}
-		}
-
-		this.animate(start, -this.sum)
-		this.option.content.append_to_style(this.option.wrapper_block, {
-			left: `-${this.sum}px`
-		})
-	}
-	animate(start, end){
-		this.option.wrapper_block.animate([
-		  {left:  start + 'px'},
-		  {left:  end + 'px'}
-		], {
-		  duration: this.option.content.data.duration, 
-		  iterations: 1,
-		});
-	}
-	change_status_index(index){
-		this.status_index = index
-	}
-
-	current_dot(){
-		if(this.option.content.data.dots){
-			let array = [...document.querySelectorAll(`.dot`)]
-			array.forEach(element => {
-				element.classList.remove('current_dot')
-			})
-			array[this.status_index].classList.add('current_dot')
-		}
-	}
-}
-
-class Feature {  
-	footer_block = document.querySelector(`.footer`)
-	wrapper_block = document.querySelector(`.wrapper`)
 	constructor(content){
 		this.content = content
+		this.elements = [...this.content.block_carusele.children] 
+
+		let line = []
+		for (let i = 0; i < this.elements.length; i++) {
+			line.push(i)
+		}
+		this.matrix.push(line)
+
+	}
+	builder(){ 
+		this.width = this.content.calculateTheWidth(this.elements)
+
+		if(this.content.data.loop){
+			this.content.clone_content(this.content.block_carusele, [...this.content.block_carusele.children], 'bottom', 2)
+
+			this.start_shift += this.width
+			this.width = this.content.calculateTheWidth([...this.content.block_carusele.children])
+			this.build_matrix('left')
+			this.build_matrix('rigth')
+		} 
+		
+		this.wrapper = this.content.wrapper([...this.content.block_carusele.children], 'wrapper')
+		this.footer = this.content.create_element('footer')
+
+		typical.styles.wrapper.left = `-${this.start_shift}px`
+		typical.styles.wrapper.width = `${this.width}px`
+		this.content.append_to_style(this.wrapper, typical.styles.wrapper)
+
+		this.content.append_to_style(this.content.block_carusele, typical.styles.id )
+		this.content.append_to_style(this.footer, typical.styles.footer)
+		
+	}
+	render(){
+		this.builder()
+		
+		this.content.add_html_block(this.content.block_carusele, this.wrapper, 'bottom')
+		this.content.add_html_block(this.content.block_carusele, this.footer, 'bottom')
+
 		this.event = new Event(this)
+		this.start_option()
+	}
+	start_option(){
+		for(let item in this.content.data){
+			if(typeof this.content.data[item] == 'boolean' && this.content.data[item] == true){
+				if(this.__proto__.hasOwnProperty(item)){
+					this[item]()	
+				}
+			} else if(typeof this.content.data[item] == 'number'){
+				if(this.__proto__.hasOwnProperty(item)){
+					this[item](this.content.data[item])
+				}
+			}
+
+		}
 	}
 	arrows(){	
-		this.content.add_html_block(this.footer_block, [], 'top', 'left')
-		this.content.add_html_block(this.footer_block, [], 'bottom', 'rigth')
+		
+		this.left = this.content.create_element('left')
+		this.rigth = this.content.create_element('rigth')
 
-		document.querySelector(`.left`).addEventListener('click', event => {
-			this.event.move_to(this.event.status_index - 1)
+		this.content.add_html_block(this.footer, this.left, 'top')
+		this.content.add_html_block(this.footer, this.rigth , 'bottom')
+
+		this.left.addEventListener('click', event => {
+			this.event.move_to(this.status_index - 1)
 		})
 
-		document.querySelector(`.rigth`).addEventListener('click', event => {
-			this.event.move_to(this.event.status_index + 1)
+		this.rigth.addEventListener('click', event => {
+			this.event.move_to(this.status_index + 1)
 		})
 	}
 	dots(){
-		let center = this.content.get_center_block(this.footer_block)
-		let dots = this.content.get_array_elements([...this.wrapper_block.children])
+		let center = this.content.get_center_block(this.footer)
+		let dots = this.content.get_array_elements(this.elements)
 		dots.classList.add('dots')
 
 		let list_dots = [...dots.children]
@@ -198,32 +195,15 @@ class Feature {
 			div.classList.add('dot')
 		})
 
-		center != -1 ? center.after(dots) : this.footer_block.appendChild(dots)
+		center != -1 ? center.after(dots) : this.footer.appendChild(dots)
 
 		this.event.current_dot()
 
 		list_dots.forEach(element => {
 			element.addEventListener('click', event => {
 				let index = list_dots.indexOf(event.target)
-				this.event.move_to(index)
-				
-				this.event.current_dot()
-			})
-		})
-	}
-	touch(){
-		this.content.add_html_block(this.content.block_carusele, [...this.wrapper_block.children], 'bottom', 'touch')
-		let list = [...document.querySelector(`.touch`).children]
-		list.forEach(div => {
-			this.content.append_to_style(div,  {
-				"width": "100%",
-				'display': 'block',})
-		})
 
-		list.forEach(element => {
-			element.addEventListener('mouseover', event => {
-				let index = list.indexOf(event.target)
-				this.event.change_img(index)
+				this.event.move_to(index)
 			})
 		})
 	}
@@ -231,38 +211,169 @@ class Feature {
 		if(i > this.content.block_carusele.childElementCount){
 			i = 0
 		}
-		
-		this.event.change_status_index(i)
-		this.event.change_img(i)
+
+		this.event.change_block(i)
+	}
+	touch(){
+		let list = this.content.get_array_elements(this.elements)
+		list.classList.add('touch')
+		let children_list = [...list.children]
+		children_list.forEach(div => {
+			this.content.append_to_style(div,  {
+				"width": "100%",
+				'display': 'block',})
+		})
+		this.content.append_to_style(list, typical.styles.touch)
+		this.content.add_html_block(this.content.block_carusele, list, 'bottom')
+
+
+		children_list.forEach(element => {
+			element.addEventListener('mouseover', event => { 
+				let index = children_list.indexOf(event.target)
+				this.event.change_block(index)
+			})
+		})
+	}
+	build_matrix(side){
+		let line = []
+		for (let i = 0; i < this.elements.length; i++) {
+			line.push(i)
+		}
+
+		if (side == 'left') {
+			this.matrix.unshift(line)
+			this.core += 1
+			
+		} else if(side == 'rigth') {
+			this.matrix.push(line)
+		}
+
+		return this.matrix
 	}
 }
 
+class Event {
+	constructor(option){
+		this.option = option
+		this.sum = this.option.start_shift
+	}
+	animate(start, end){
+		this.option.wrapper.animate([
+		  {left:  start + 'px'},
+		  {left:  end + 'px'}
+		], {
+		  duration: this.option.content.data.duration,
+		  iterations: 1,
+		});
+	}
+	change_status(index){
+		this.option.status_index = index
+	}
+	change_block(index){
+		this.sum = this.option.start_shift
+		this.sum += [...this.option.wrapper.children][index].offsetWidth * index 
+		this.option.content.append_to_style(this.option.wrapper, {
+			left: `-${this.sum}px`
+		})
+		this.change_status(index)
+		this.current_dot()
+	}
+	move_to(new_index){ 
+		let start = -this.sum
+		let step = Math.abs((this.option.status_index - new_index)) 
+		
+		if (new_index < this.option.status_index) {
+			if(!this.option.content.data.loop){ 
+				if (new_index < 0) {
+					return this.sum
+				} else {
+					new_index
+				}
+			} else if (this.option.content.data.loop) {
+				if(new_index < 0 ){
+					this.option.core -= 1
+					let last = this.option.matrix[this.option.core].length - 1
+					new_index = last
 
-class Carusel{
+					setTimeout(() => this.direction_to('left'), 0)
+					
+				} else {
+					new_index
+				}
+			}	
+			this.sum = this.sum - (this.option.elements[new_index].offsetWidth * step)		
+		}
+
+		else if(new_index > this.option.status_index){ 
+			if (!this.option.content.data.loop) {
+				if(new_index >= this.option.elements.length){
+					return this.sum 
+				} else {
+					new_index
+				}
+			} else if(this.option.content.data.loop){
+				if(new_index >= this.option.elements.length){
+					this.option.core += 1
+					let first = 0
+					new_index = first 
+
+					setTimeout(() => this.direction_to('rigth'), 0)
+				} 
+			}
+	
+				this.sum += (this.option.elements[new_index].offsetWidth * step)
+		}
+
+		this.change_status(new_index)
+		this.current_dot()
+
+		this.animate(start, -this.sum)
+		this.option.content.append_to_style(this.option.wrapper, {
+			left: `-${this.sum}px`
+		})
+	}
+
+	current_dot(){
+		if(this.option.content.data.dots){
+			let array = [...document.querySelectorAll(`.dot`)]
+			let index = this.option.status_index
+			array.forEach(element => {
+				element.classList.remove('current_dot')
+			})
+	
+			array[index].classList.add('current_dot')
+		}
+	}
+
+	direction_to(side){ 
+		let shift = 0
+		if (side == 'rigth') {
+			this.option.core -= 1
+			shift = this.option.start_shift
+		} else if(side == 'left'){
+			this.option.core += 1
+			shift = (this.option.start_shift + this.option.content.calculateTheWidth(this.option.elements))  - [...this.option.elements][0].offsetWidth 
+		}
+		
+
+		this.option.content.append_to_style(this.option.wrapper, {
+			left: `-${shift}px`
+		})
+		this.sum = shift
+	}
+
+}
+
+class Carusel {
 	constructor(id, data){
 		this.name_id = id
 		this.data = this.replaceDataset(data)
 		this.block_carusele = document.getElementById(this.name_id)
 	}
-	append_style_to(elements, style){
-		let block = [...document.querySelectorAll(elements)]
-		block.forEach(div => {
-			this.append_to_style(div, style)
-		})
-	}
-	start(){
-		this.display_carusel(this.block_carusele)
-		let s = new Feature(this)
 
-		for(let item in this.data){
-			if(typeof this.data[item] == 'boolean' && this.data[item] == true){
-				s[item]()
-			} else if(typeof this.data[item] == 'number'){
-				if(s.__proto__.hasOwnProperty(item)){
-					s[item](this.data[item])
-				}
-			} 
-		}
+	start(){
+		this.s = new Feature(this)
+		this.s.render()
 	}
 }
 
